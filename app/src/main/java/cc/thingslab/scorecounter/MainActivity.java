08c68c94c744b456bccb60cf313488061ab3d472
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-06-06 18:44:23 +0200

    team A can score now

--- app/src/main/java/cc/thingslab/scorecounter/MainActivity.java
+++ app/src/main/java/cc/thingslab/scorecounter/MainActivity.java
@@ -2,14 +2,38 @@ package cc.thingslab.scorecounter;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    int scoreA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addThreeToTeamA(View view) {
+        scoreA += 3;
+        displayScoreA(scoreA);
+    }
+
+    public void addTwoToTeamA(View view) {
+        scoreA += 2;
+        displayScoreA(scoreA);
+    }
+
+    public void addOneToTeamA(View view) {
+        scoreA += 1;
+        displayScoreA(scoreA);
+    }
+
+    private void displayScoreA(int score) {
+        TextView scoreTeamA = (TextView) findViewById(R.id.team_a_score);
+        scoreTeamA.setText(String.valueOf(score));
     }
 
 }
