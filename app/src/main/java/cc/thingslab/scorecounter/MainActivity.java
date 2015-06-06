0960eac7f9f1bca00563b9b8bda898d3be685263
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-06-06 18:51:45 +0200

    two teams against each other

--- app/src/main/java/cc/thingslab/scorecounter/MainActivity.java
+++ app/src/main/java/cc/thingslab/scorecounter/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends ActionBarActivity {
 
     int scoreA = 0;
+    int scoreB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +19,42 @@ public class MainActivity extends ActionBarActivity {
 
     public void addThreeToTeamA(View view) {
         scoreA += 3;
-        displayScoreA(scoreA);
+        updateScoreA();
     }
 
     public void addTwoToTeamA(View view) {
         scoreA += 2;
-        displayScoreA(scoreA);
+        updateScoreA();
     }
 
     public void addOneToTeamA(View view) {
         scoreA += 1;
-        displayScoreA(scoreA);
+        updateScoreA();
     }
 
-    private void displayScoreA(int score) {
+    private void updateScoreA() {
         TextView scoreTeamA = (TextView) findViewById(R.id.team_a_score);
-        scoreTeamA.setText(String.valueOf(score));
+        scoreTeamA.setText(String.valueOf(scoreA));
+    }
+
+    public void addThreeToTeamB(View view) {
+        scoreB += 3;
+        updateScoreB();
+    }
+
+    public void addTwoToTeamB(View view) {
+        scoreB += 2;
+        updateScoreB();
+    }
+
+    public void addOneToTeamB(View view) {
+        scoreB += 1;
+        updateScoreB();
+    }
+
+    private void updateScoreB() {
+        TextView scoreTeamB = (TextView) findViewById(R.id.team_b_score);
+        scoreTeamB.setText(String.valueOf(scoreB));
     }
 
 }
